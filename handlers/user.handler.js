8b2d516cede6448168feb8ba42f389ea0647f697
Bayorwor <bayorwor60@gmail.com>
2022-04-22 15:24:20 +0000

    error handler

---
+++ handlers/user.handler.js
@@ -0,0 +1,18 @@
+const userhandler ={}
+
+userhandler.handleErrors = (error)=>{
+    let errorData = {username:"", password:""};
+
+    if(error.code ===11000){
+        errorData.username = "the username is not available";
+        return errorData;
+    };
+    if (error.message.includes("user validation failed")){
+      
+        Object.values(error.errors).forEach(({properties})=>{
+            errorData[properties.path]= properties.message;
+        })
+    }
+}
+
+module.exports =userhandler
